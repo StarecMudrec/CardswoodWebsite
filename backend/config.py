7967b277a5e0c08a9cb0faa764a331da9ff8df3a
michaellebedevcuber <michaellebedevcuber@gmail.com>
2025-05-19 23:04:08 +0000

    secret key )

--- backend/config.py
+++ backend/config.py
@@ -8,3 +8,4 @@ class Config:
     BOT_TOKEN_HASH = sha256("7505659847:AAF3AoBU0HFInjsPPOqz4KaPmX3M15l9f1U".encode())  # Replace with your actual token
     SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://postgres:postgres@db:5432/cards"  # SQLite database
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get("SECRET_KEY")
