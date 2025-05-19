f499fcfb425f4b7ee649fc58f5543ff9799ee2b4
michaellebedevcuber <michaellebedevcuber@gmail.com>
2025-05-19 22:15:34 +0000

    asdfasd

--- backend/config.py
+++ backend/config.py
@@ -5,6 +5,6 @@ from hashlib import sha256
 class Config:
     # Configuration
     JWT_SECRET_KEY = "COOL"  # Replace with your actual secret key
-    BOT_TOKEN_HASH = sha256("7505659847:AAF3AoBU0HFInjsPPOqz4KaPmX3M15l9f1U".encode())  # Replace with your actual token
-    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://postgres:123wow123@db:5432/cards"  # SQLite database
+    BOT_TOKEN_HASH = sha256("7505659847:AAF3AoBU0HFInjsPPOqz4KaPmX3M15l9f1U".encode()) # Replace with your actual token
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI', 'postgresql+psycopg2://postgres:123wow123@db:5432/cards') # SQLite database
     SQLALCHEMY_TRACK_MODIFICATIONS = False
