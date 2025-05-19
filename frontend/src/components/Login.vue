a7a35e830be86a1e8014ed3f9b0c7d59dbcdb4f8
michaellebedevcuber <michaellebedevcuber@gmail.com>
2025-05-19 22:40:24 +0000

    пониже передвинул его

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -53,7 +53,7 @@ export default {
   background-color: var(--card-bg);
   border-radius: 8px;
   border: 1px solid var(--border-color);
-  margin: 40px auto;
+  margin: 80px auto;
   text-align: center;
 }
 
