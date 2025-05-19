58a34687422811f939e8ec4ef5b036a82f577066
michaellebedevcuber <michaellebedevcuber@gmail.com>
2025-05-19 21:56:37 +0000

    changed postgres password and ports

--- backend/config.py
+++ backend/config.py
@@ -6,5 +6,5 @@ class Config:
     # Configuration
     JWT_SECRET_KEY = "COOL"  # Replace with your actual secret key
     BOT_TOKEN_HASH = sha256("7505659847:AAF3AoBU0HFInjsPPOqz4KaPmX3M15l9f1U".encode())  # Replace with your actual token
-    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://postgres:postgres@db:5432/cards"  # SQLite database
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://postgres:]7YD8£73vIo(@db:5432/cards"  # SQLite database
     SQLALCHEMY_TRACK_MODIFICATIONS = False
