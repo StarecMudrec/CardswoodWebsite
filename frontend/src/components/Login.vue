ceed48ec19fdf18e67eff32fb76bb3a65f812d56
michaellebedevcuber <michaellebedevcuber@gmail.com>
2025-05-23 19:15:07 +0000

    йоуйоуйоуу

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -52,7 +52,7 @@ export default {
   top: 0;
   left: 0;
   width: 100%;
-  height: 700px;
+  height: 100%;
   background-image: url('/background.jpg');
   background-size: cover;
   background-position: center 100%; /* Position the vertical center 80% down from the top, center horizontally */
