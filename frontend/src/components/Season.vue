6ac2f37f5b1433e2c17b4338f5b2ff9f71e9e940
michaellebedevcuber <michaellebedevcuber@gmail.com>
2025-05-25 00:22:28 +0000

    asdasd

--- frontend/src/components/Season.vue
+++ frontend/src/components/Season.vue
@@ -230,7 +230,7 @@ export default {
   border: none;
   color: var(--text-color);
   cursor: pointer;
-  font-size: 22;
+  font-size: 22px;
   font-weight: 500;
   padding: 5px 0;
   position: relative;
