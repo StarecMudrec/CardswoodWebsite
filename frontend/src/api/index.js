099c39a4c431ca983dfa5337ef24d7f4ba265179
michaellebedevcuber <michaellebedevcuber@gmail.com>
2025-05-28 19:48:17 +0000

    фывфыв

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -84,7 +84,6 @@ export const deleteCard = async (cardId) => {
   const response = await fetch(`/api/cards/${cardId}`, {
     method: 'DELETE'
   });
-  console.log(`Fetching ${method} ${url}`);
   if (!response.ok) throw new Error('Failed to delete card');
 };
 
