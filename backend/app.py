bb93b76a87d6164511fd20ec4346796ca5d09cd5
STARECMUDREC <117379069+StarecMudrec@users.noreply.github.com>
2025-08-06 16:55:08 +0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -544,12 +544,12 @@ def update_card_image(card_uuid):
 @app.route("/api/season_info/<int:season_id>")
 def get_season_info(season_id):  
     try:
-        season_num = season_id
+        season_num = int(season_id)
         
         season_info = {
             'id': season_num,
             'uuid': season_num,
-            'name': "Season" + season_num
+            'name': "Season" + str(season_num)
         }
         
         return jsonify(season_info), 200
