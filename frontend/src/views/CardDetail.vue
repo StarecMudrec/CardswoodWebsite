7b3165bcbde728fd78a16fe0cd6c9ad36dd2dedb
STARECMUDREC <117379069+StarecMudrec@users.noreply.github.com>
2025-08-08 21:38:43 +0300

    Update CardDetail.vue

--- frontend/src/views/CardDetail.vue
+++ frontend/src/views/CardDetail.vue
@@ -1227,7 +1227,12 @@
   .no-comments {
     color: #666;
     font-style: italic;
-    pointer-events: none;
+    -webkit-touch-callout: none;
+    -webkit-user-select: none;
+    -khtml-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
   }
 
   .comments-list {
