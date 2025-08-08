ada17e839c6644a74c88eed16e7449ec15b0c453
STARECMUDREC <117379069+StarecMudrec@users.noreply.github.com>
2025-08-08 21:32:09 +0300

    Revert "Update CardDetail.vue"
    This reverts commit ccd1b1b1332cd28798f9442dd1f0ac4184dffe8f.

--- frontend/src/views/CardDetail.vue
+++ frontend/src/views/CardDetail.vue
@@ -32,7 +32,7 @@
         </div>
       </div>
 
-      <transition :name="showTransition ? transitionName : ''" mode="in-out">
+      <transition :name="showTransition ? transitionName : ''" mode="out-in">
         <div :key="card.id" class="card-detail-container">
           <div class="card-detail">
             <div class="card-image-container">
