5d3dd9dfeb53020c03fd44b5d29bde86374d6170
STARECMUDREC <117379069+StarecMudrec@users.noreply.github.com>
2025-12-15 21:26:24 +0300

    .env

--- backend/config.py
+++ backend/config.py
@@ -1,15 +1,17 @@
 import os
 from hashlib import sha256
 from sqlalchemy import create_engine
+from dotenv import load_dotenv
 
+load_dotenv()
 
 class Config:
     # Configuration
     JWT_SECRET_KEY = "COOL"  # Replace with your actual secret key
-    BOT_TOKEN_HASH = sha256(os.environ.get("BOT_TOKEN").encode())  # Replace with your actual token
+    BOT_TOKEN_HASH = sha256(os.getenv("BOT_TOKEN").encode())  # Replace with your actual token
     SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://postgres:postgres@db:5432/cards"  # SQLite database
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.environ.get("SECRET_KEY")
+    SECRET_KEY = os.getenv("SECRET_KEY")
     
     # For SQLite over TCP proxy
     SQLITE_DB_PATH = "/app/db/offcardswood.db"  # Mounted path in container
