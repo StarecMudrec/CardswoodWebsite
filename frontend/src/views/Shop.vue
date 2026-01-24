c51350309f14013dbdff29114e3553cc52f6408b
STARECMUDREC <117379069+StarecMudrec@users.noreply.github.com>
2026-01-24 16:26:00 +0300

    Update Shop.vue

--- frontend/src/views/Shop.vue
+++ frontend/src/views/Shop.vue
@@ -70,7 +70,7 @@
                 <span class="cart-item-price">{{ item.price.toLocaleString('ru-RU') }} ₽</span>
               </div>
               <button class="remove-btn" @click="removeFromCart(item.id)">
-                <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41Z" fill="currentColor"/>
                 </svg>
               </button>
@@ -555,7 +555,7 @@ export default {
   display: flex;
   justify-content: space-between;
   flex: 1;
-  margin-right: 16px;
+  margin-right: 10px;
 }
 
 .cart-item-name {
@@ -577,6 +577,7 @@ export default {
   padding: 4px;
   border-radius: 4px;
   transition: color 0.3s ease;
+  display: flex;
 }
 
 .remove-btn:hover {
